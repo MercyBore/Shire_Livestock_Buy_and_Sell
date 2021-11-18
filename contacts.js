cb042b32a867f2029cb758eb04c5dc808982cff7
wabwile-memory <memory.wabwile@student.moringaschool.com>
2021-11-18 09:52:27 +0300

    input error message on the form

--- contacts.js
+++ contacts.js
@@ -1,6 +1,7 @@
-/*const name = document.getElementById('name')
-const name = document.getElementById('email')
-const name = document.getElementById('mess')
+
+/*var name = document.getElementById('name')
+const email = document.getElementById('email')
+const mess = document.getElementById('mess')
 const errorElement = document.getElementById('error')
 
 const form = document.getElementById('form')
@@ -16,3 +17,48 @@ form.addEventListener('submit', (e) => {
     }
    
 }); */
+
+// var form = document.getElementById("form");
+
+
+// form.addEventListener("submit",(e) =>{
+//     e.preventDefault();
+    
+//     var name = document.getElementById("name").value;
+    
+//     if (name ===""){
+//         document.getElementById("name-error").style.display = "block";
+//     } else{
+//         document.getElementById("name-error").innerHTML = "";
+//         form.submit();
+//     }
+// });
+
+
+
+/*var btnSubmit = document.getElementById('submit')
+
+btnSubmit.addEventListener('click', (e)=>{
+ e.preventDefault()
+
+
+ var name = document.getElementById("name").value;
+
+     if (name === ""){
+        document.getElementById("name-error").style.display = "block";
+
+    } else{
+        document.getElementById("name-error").innerHTML = "";
+
+        
+        console.log(name)
+
+        name.textcontent = ''
+        // console.log('else')
+    }
+
+
+
+    console.log('Btn clicked')
+})
+*/
